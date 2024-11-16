8c8637fea1d6c40a83ca204536266f34de4caf3f
M Zeeshan <zashraf080@gmail.com>
2024-11-16 20:32:41 +0500

    video player screen added

---
+++ app/src/main/java/com/example/zeeshanassignment/activity/VideoPlayerActivity.kt
@@ -0,0 +1,49 @@
+package com.example.zeeshanassignment.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.exoplayer.ExoPlayer
+import android.net.Uri
+import android.view.View
+
+import androidx.media3.common.MediaItem
+import com.example.zeeshanassignment.R
+import com.example.zeeshanassignment.databinding.ActivityVideoPlayerBinding
+
+class VideoPlayerActivity : AppCompatActivity() {
+    private lateinit var exoPlayer: ExoPlayer
+    private lateinit var binding: ActivityVideoPlayerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVideoPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       // supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        supportActionBar?.hide()
+
+        // Hide the system UI for fullscreen experience
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
+
+        // Initialize ExoPlayer
+        exoPlayer = ExoPlayer.Builder(this).build()
+        binding.playerView.player = exoPlayer
+
+        // Set the video URL
+        val videoUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+        val mediaItem = MediaItem.fromUri(Uri.parse(videoUrl))
+        exoPlayer.setMediaItem(mediaItem)
+
+        // Prepare and play the video
+        exoPlayer.prepare()
+        exoPlayer.playWhenReady = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        exoPlayer.release() // Release the player when not in use
+    }
+}
